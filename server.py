0f49f2a332222968ab30073750ae6611cbddc26c
Nilotpal Chanda <nilotpalchanda14@gmail.com>
2019-09-07 22:12:54 +0530

    chanhes1

--- server.py
+++ server.py
@@ -1,8 +1,16 @@
 from flask import Flask, flash, render_template, redirect, url_for, request
 from werkzeug.utils import secure_filename
+from flask_pymongo import PyMongo
 from flask import jsonify
+import urllib.parse
 import os
-import db as d
+
+app = Flask(__name__)
+
+
+app.config['MONGO_DBNAME'] = 'python_api' # name of database on mongo
+app.config['MONGO_URI'] = 'mongodb://test:' + urllib.parse.quote_plus("test123") + '@ds219078.mlab.com:19078/python_api?retryWrites=false'
+mongo = PyMongo(app)
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -19,7 +27,7 @@ def face_upload():
     target = os.path.join(APP_ROOT, 'face-images/')  #folder path
     if not os.path.isdir(target):
             os.mkdir(target)     # create folder if not exits
-    face_db_table = d.mongo.db.faces  # database table name
+    face_db_table = mongo.db.faces  # database table name
     if request.method == 'POST':
         for upload in request.files.getlist("face_image"): #multiple image handel
             filename = secure_filename(upload.filename)
@@ -31,7 +39,7 @@ def face_upload():
 
 @app.route('/alldata', methods=['GET'])
 def get_all_data():
-  data = d.mongo.db.faces
+  data = mongo.db.faces
   output = []
   for s in data.find():
     output.append({'name' : s['name'], 'destination' : s['destination']})
@@ -39,11 +47,10 @@ def get_all_data():
 
 @app.route('/alldata/<name>', methods=['GET'])
 def get_one_star(name):
-  star = d.mongo.db.faces
+  star = mongo.db.faces
   s = star.find_one({'name' : name})
   if s:
     output = {'name' : s['name'], 'destination' : s['destination']}
   else:
     output = "No such name"
   return jsonify({'result' : output})
-
